da8e2b9070d11cfb7193544d43f9cc7151d36364
Diana Priediena <dian6210@stud.kea.dk>
2021-02-24 13:54:50 +0100

    Added click away the button

--- js/series.js
+++ js/series.js
@@ -1,3 +1,10 @@
-document.querySelector("#genre-button").addEventListener("click", function () {
-  document.querySelector("#genre-dropdown").classList.toggle("hidden");
+document
+  .querySelector("#genre-button")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+    document.querySelector("#genre-dropdown").classList.toggle("hidden");
+  });
+
+window.addEventListener("click", function () {
+  document.querySelector("#genre-dropdown").classList.add("hidden");
 });
